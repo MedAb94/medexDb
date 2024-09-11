51fed53a55bf618f607131fba64abb8bfbde03ee
medab <medab.vall@gmail.com>
2024-09-11 11:43:49 +0000

    fix psr

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class country extends Model
+class Country extends Model
 {
     use SoftDeletes;
 
